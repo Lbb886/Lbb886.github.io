68c4cdf2fc9ac88d1d4dcd0f56e15d6ff859473b
Lbb886 <107664455+Lbb886@users.noreply.github.com>
2024-09-28 09:47:03 +0000

    github action update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/f5103fb2.html"];function toRandomPost(){
+var posts=["posts/f5103fb2.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
